053d61f3a89a458e864e51aa16b48ddb100e6cdf
kishtarn555 <hec_montemoon@hotmail.com>
2022-12-15 05:41:23 -0600

    toolkit 2

--- js/frontend.js
+++ js/frontend.js
@@ -118,4 +118,5 @@ function recalcDimensions() {
     $("#desktopView").addClass( "d-none" );
     $("#desktopView").addClass( "d-lg-block");
     $("#phoneView").removeClass( "position-absolute" );
+    $("#loadingModal").remove();
   });
